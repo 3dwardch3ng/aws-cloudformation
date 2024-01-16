2b67c6e5ac1179d0644490c674d1165cc6037c40
Edward Cheng <edward@cheng.sydney>
2024-01-17 07:36:24 +1100

    Fix Nat Instance template

--- s3-static-files/iptables/firewall-init.sh
+++ s3-static-files/iptables/firewall-init.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# iptables / ip6tables single-host firewall script
+echo "iptables / ip6tables single-host firewall script"
 
-# Define your command variables
-ipt="/sbin/ip6tables"
+echo "Define your command variables"
+ipt="/sbin/iptables"
 ipt6="/sbin/ip6tables"
 
-# Flush all rules and delete all chains
-# because it is best to startup cleanly
+echo "Flush all rules and delete all chains"
+echo "because it is best to startup cleanly"
 $ipt -F
 $ipt -X
 $ipt -t nat -F
@@ -17,17 +17,17 @@ $ipt -t mangle -X
 $ipt6 -F
 $ipt6 -X
 
-# Zero out all counters, again for
-# a clean start
+echo "Zero out all counters, again for"
+echo "a clean start"
 $ipt -Z
 $ipt -t nat -Z
 $ipt -t mangle -Z
 $ipt6 -Z
 
-# Default policies: deny all incoming
-# Unrestricted outgoing
+echo "Default policies: deny all incoming"
+echo "Unrestricted outgoing"
 $ipt -P INPUT DROP
-$ipt -P FORWARD DROP
+$ipt -P FORWARD ACCEPT
 $ipt -P OUTPUT ACCEPT
 $ipt -t nat -P OUTPUT ACCEPT
 $ipt -t nat -P PREROUTING ACCEPT
@@ -35,84 +35,85 @@ $ipt -t nat -P POSTROUTING ACCEPT
 $ipt -t mangle -P PREROUTING ACCEPT
 $ipt -t mangle -P POSTROUTING ACCEPT
 $ipt6 -P INPUT DROP
-$ipt6 -P FORWARD DROP
+$ipt6 -P FORWARD ACCEPT
 $ipt6 -P OUTPUT ACCEPT
 
-# Must allow loopback interface
+echo "Must allow loopback interface"
 $ipt -A INPUT -i lo -j ACCEPT
 $ipt6 -A INPUT -i lo -j ACCEPT
 
-# Reject connection attempts not initiated from the host
+echo "Reject connection attempts not initiated from the host"
 # $ipt -A INPUT -p tcp --syn -j DROP
 # $ipt6 -A INPUT -p tcp --syn -j DROP
 
-# Allow return connections initiated from the host
+echo "Allow return connections initiated from the host"
 $ipt -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 $ipt6 -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 
-# Accept important ICMP packets. It is not a good
-# idea to completely disable ping; networking
-# depends on ping
+echo "Accept important ICMP packets. It is not a good"
+echo "idea to completely disable ping; networking"
+echo "depends on ping"
 $ipt -A INPUT -p icmp --icmp-type echo-request -j ACCEPT
 $ipt -A INPUT -p icmp --icmp-type time-exceeded -j ACCEPT
 $ipt -A INPUT -p icmp --icmp-type destination-unreachable -j ACCEPT
 
-# Accept all ICMP v6 packets
+echo "Accept all ICMP v6 packets"
 $ipt6 -A INPUT -p ipv6-icmp -j ACCEPT
 
-# The previous lines define a simple firewall
-# that does not restrict outgoing traffic, and
-# allows incoming traffic only for established sessions
+echo "The previous lines define a simple firewall"
+echo "that does not restrict outgoing traffic, and"
+echo "allows incoming traffic only for established sessions"
 
-# The following rules are optional to allow external access
-# to services. Adjust port numbers as needed for your setup
+echo "The following rules are optional to allow external access"
+echo "to services. Adjust port numbers as needed for your setup"
 
-# Use this rule when you accept incoming connections
-# to services, such as SSH and HTTP
-# This ensures that only SYN-flagged packets are
-# allowed in
-# Then delete '$ipt -A INPUT -p tcp --syn -j DROP'
+echo "Use this rule when you accept incoming connections"
+echo "to services, such as SSH and HTTP"
+echo "This ensures that only SYN-flagged packets are"
+echo "allowed in"
+echo "Then delete '$ipt -A INPUT -p tcp --syn -j DROP'"
 $ipt -A INPUT p tcp ! --syn -m state --state NEW -j DROP
 
-# Optional rules to allow other LAN hosts access
-# to services. Delete $ipt6 -A INPUT -p tcp --syn -j DROP
+echo "Optional rules to allow other LAN hosts access"
+echo "to services. Delete $ipt6 -A INPUT -p tcp --syn -j DROP"
 
-# Allow DHCPv6 from LAN only
+echo "Allow DHCPv6 from LAN only"
 # $ipt6 -A INPUT -m state --state NEW -m udp -p udp -s fe80::/10 --dport 546 -j ACCEPT
 
-# Allow connections from SSH clients
+echo "Allow connections from SSH clients"
 $ipt6 -A INPUT -m state --state NEW -m tcp -p tcp --dport 22 -j ACCEPT
 
-# Allow HTTP and HTTPS traffic
+echo "Allow HTTP and HTTPS traffic"
 # $ipt6 -A INPUT -m state --state NEW -m tcp -p tcp --dport 80 -j ACCEPT
 # $ipt6 -A INPUT -m state --state NEW -m tcp -p tcp --dport 443 -j ACCEPT
 
-# NAT request forwarding for PiHole instance
+echo "NAT request forwarding for PiHole instance"
 
-# Allow TCP/UDP traffic from whitelisted IPv4s
+$ipt -P FORWARD ACCEPT
+$ipt6 -P FORWARD ACCEPT
+$ipt -t nat -A POSTROUTING -o etX0 -j MASQUERADE
+$ipt6 -t nat -A POSTROUTING -o etX0 -j MASQUERADE
+
+
+echo "Allow TCP/UDP traffic from whitelisted IPv4s"
 input="/etc/iptables/whitelisted-ipv4.txt"
 while IFS= read -r line
 do
-  $ipt -t nat -A POSTROUTING -o eth0 -s $line -j MASQUERADE
   $ipt -s $line -F FORWARD
 done < "$input"
 
-# Allow TCP/UDP traffic from whitelisted IPv6s
+echo "Allow TCP/UDP traffic from whitelisted IPv6s"
 input="/etc/iptables/whitelisted-ipv6.txt"
 while IFS= read -r line
 do
-  $ipt6 -t nat -A POSTROUTING -o etX0 -s $line -j MASQUERADE
   $ipt6 -s $line -F FORWARD
 done < "$input"
 
-
-# Allow TCP traffic from whitelisted MAC addresses
+echo "Allow TCP traffic from whitelisted MAC addresses"
 input="/etc/iptables/whitelisted-mac.txt"
 while IFS= read -r line
 do
-  $ipt -t nat -A POSTROUTING -o eth0 -m mac --mac-source $line -j MASQUERADE
   $ipt -m mac --mac-source $line -F FORWARD
-  $ipt6 -t nat -A POSTROUTING -o eth0 -m mac --mac-source $line -j MASQUERADE
   $ipt6 -m mac --mac-source $line -F FORWARD
 done < "$input"
 
